bc8752c511173d1471a73b6c4ea5de1eea6c4fbd
Nihir14 <nihirverma14@gmail.com>
2024-10-03 15:11:34 +0530

    update

---
+++ CHAPTER 2 - OOPs/01 Basics/Modifiers/NeedOfModifiers.java
@@ -0,0 +1,13 @@
+public class NeedOfModifiers {
+    static final int a = 10;
+    public static void main(String[] args) {
+        // System.out.println(a);
+        // System.out.println(a);
+    }
+}
+
+class Sample {
+    public static void main(String[] args) {
+        System.out.println(NeedOfModifiers.a);
+    }
+}
---
+++ CHAPTER 2 - OOPs/01 Basics/Modifiers/p2/C.java
@@ -0,0 +1,22 @@
+package p2;
+import p1.A;
+
+public class C extends A {
+
+    public static void main(String[] args) {
+        
+        A a1 = new A();
+        // System.out.println("a :"+ a1.a);
+        // System.out.println("b :"+ a1.b);
+        // System.out.println("c :"+ a1.c);
+        // System.out.println("d :"+ a1.d);
+
+        C c1 = new C();
+        // System.out.println("a :"+ c1.a);
+        // System.out.println("b :"+ c1.b);
+        System.out.println("c :"+ c1.c);
+        System.out.println("d :"+ c1.d);
+
+
+    }
+}
